87e7058fbca1ba2da4252a1bd2573a7d3ce47e0c
Tanya <tanaybrown@gmail.com>
2022-04-21 10:29:42 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const element = document.getElementById("main");
+element.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Tanya is the champion"
